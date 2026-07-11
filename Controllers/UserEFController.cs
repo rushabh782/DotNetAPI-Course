bbe4e7d3c412f150778da5ff0dd1068c402f9934
Rushabh Patel <rushabh_patel@idealake.com>
2026-07-11 20:00:32 +0530

    added controller for EF

---
+++ Controllers/UserEFController.cs
@@ -0,0 +1,99 @@
+
+using DotnetAPI.Data;
+using DotnetAPI.Dtos;
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserEFController : ControllerBase
+{
+    DataContextEF _entityFramework;
+    public UserEFController(IConfiguration config)
+    {
+        _entityFramework = new DataContextEF(config);
+    }
+
+
+
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers()
+    {
+        IEnumerable<User> users = _entityFramework.Users.ToList<User>();
+        return users;   
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        User? user = _entityFramework.Users.Where(u => u.UserId == userId).FirstOrDefault<User>();
+
+        if(user != null)
+        {
+          return user;   
+        }
+        throw new Exception("User not found");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(UserToAddDto user)
+    {
+        User userDb = new User();       
+
+          userDb.FirstName = user.FirstName; 
+          userDb.LastName = user.LastName; 
+          userDb.Email = user.Email; 
+          userDb.Gender = user.Gender; 
+          userDb.Active = user.Active; 
+          
+          if(_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+        throw new Exception("Failed to add user");
+    }
+
+    [HttpPut("EditUser")]
+    public IActionResult EditUser(User user)
+    {
+        User? userDb = _entityFramework.Users.Where(u => u.UserId == user.UserId).FirstOrDefault<User>();
+
+        if(userDb != null)
+        {
+          userDb.FirstName = user.FirstName; 
+          userDb.LastName = user.LastName; 
+          userDb.Email = user.Email; 
+          userDb.Gender = user.Gender; 
+          userDb.Active = user.Active; 
+          
+          if(_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+        throw new Exception("Failed to update user");
+        }
+        throw new Exception("User not found");
+    }
+
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId)
+    {
+       User? userDb = _entityFramework.Users.Where(u => u.UserId == userId).FirstOrDefault<User>();
+
+        if(userDb != null)
+        {
+            _entityFramework.Users.Remove(userDb);
+
+          if(_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+        throw new Exception("Failed to delete user");
+        }
+        throw new Exception("User not found");
+    }
+}
